182d56615610986202bc12dea2526c52c8af2402
Buash Ang <hoangvanquocanh1010@gmail.com>
2025-06-05 09:35:40 +0700

    Update and rename tiktokgolike to tiktokgolike.py

--- tiktokgolike.py
+++ tiktokgolike.py
@@ -20,7 +20,7 @@ from rich.console import Console
 from rich.text import Text
 def kiem_tra_mang():
     try:
-        socket.create_connection(("8.8.8.8", 53), timeout=10)
+        socket.create_connection(("8.8.8.8", 63), timeout=30)
     except OSError:
         print("Mạng không ổn định hoặc bị mất kết nối. Vui lòng kiểm tra lại mạng.")
 kiem_tra_mang()
