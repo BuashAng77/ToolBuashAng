554ae1be44f9ad46139f49b31e7680f87d764323
Buash Ang <hoangvanquocanh1010@gmail.com>
2025-06-17 13:30:23 +0700

    Update key.py

--- key.py
+++ key.py
@@ -1,8 +1,10 @@
 {
-  "key": "BUASHANGDZAI-XYZ123456775",
-  "created_at": "2025-06-17T12:00:00",
-  "expires_at": "2025-07-18T00:00:00",
-  "max_devices": 2,
-  "devices": ["device_id_1", "device_id_2"],
-  "status": "active"
+  "key": [
+    {
+      "key": "BUASHANGDZAI-XYZ123456775",
+      "created_at": "2025-06-17T12:00:00",
+      "expires_at": "2025-07-18T00:00:00",
+      "status": "active"
+    }
+  ]
 }
