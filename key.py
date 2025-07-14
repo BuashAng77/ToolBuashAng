4e761b31d53a1d4f38d6b8ce4126bece1426426c
Buash Ang <hoangvanquocanh1010@gmail.com>
2025-07-14 12:28:16 +0700

    Update key.py

--- key.py
+++ key.py
@@ -1,102 +1,22 @@
-[
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA1-AZL387264321",
-        "created_at": "2025-07-14T10:00:00",
-        "expires_at": "2025-07-17T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA2-XYZ987654321",
-        "created_at": "2025-06-18T12:00:00",
-        "expires_at": "2025-07-18T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA3-XYZ456789123",
-        "created_at": "2025-06-19T12:00:00",
-        "expires_at": "2025-07-19T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA4-XYZ321654987",
-        "created_at": "2025-06-20T12:00:00",
-        "expires_at": "2025-07-20T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA5-XYZ789123456",
-        "created_at": "2025-06-21T12:00:00",
-        "expires_at": "2025-07-21T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA6-XYZ654321789",
-        "created_at": "2025-06-22T12:00:00",
-        "expires_at": "2025-07-22T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA7-XYZ123987654",
-        "created_at": "2025-06-23T12:00:00",
-        "expires_at": "2025-07-23T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA8-XYZ456123789",
-        "created_at": "2025-06-24T12:00:00",
-        "expires_at": "2025-07-24T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA9-XYZ987321654",
-        "created_at": "2025-06-25T12:00:00",
-        "expires_at": "2025-07-25T00:00:00",
-        "status": "active"
-      }
-    ]
-  },
-  {
-    "key": [
-      {
-        "key": "BUASHANGDZA0-XYZ654789123",
-        "created_at": "2025-06-26T12:00:00",
-        "expires_at": "2025-07-26T00:00:00",
-        "status": "active"
-      }
-    ]
-  }
-]
+{
+  "key": [
+    {
+      "key": "BUASHANGDZA1-AZL387264321",
+      "created_at": "2025-07-14T10:00:00",
+      "expires_at": "2025-07-17T00:00:00",
+      "status": "active"
+    },
+    {
+      "key": "BUASHANGDZA2-XYZ987654321",
+      "created_at": "2025-06-18T12:00:00",
+      "expires_at": "2025-07-18T00:00:00",
+      "status": "active"
+    },
+    {
+      "key": "BUASHANGDZA3-XYZ456789123",
+      "created_at": "2025-06-19T12:00:00",
+      "expires_at": "2025-07-19T00:00:00",
+      "status": "active"
+    }
+  ]
+}
